822daad68681456afbe95f0fbf334f5d01937f00
Felipe Pires Soares <felipe.pires.soaresti@gmail.com>
2026-09-15 23:48:47 -0300

    fix(iam): trust OIDC para GitHub Environment producao

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -45,7 +45,20 @@ resource "aws_iam_role" "github" {
             "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${each.value.name}:ref:refs/heads/main"
           }
         }
-      }
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${each.value.name}:environment:${var.environment}"
+          }
+        }
+      },
     ]
   })
 
